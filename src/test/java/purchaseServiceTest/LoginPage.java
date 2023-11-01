9d7e327d890bfc21e168ece130844eb8efcbeffe
bprysiazhnyi <bodacuk@gmail.com>
2023-11-01 11:19:42 +0200

    First commit

---
+++ src/test/java/purchaseServiceTest/LoginPage.java
@@ -0,0 +1,20 @@
+package purchaseServiceTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginPage extends PurchaseServiceTests{
+
+@Test
+    public void clickOnLoginButton(){
+    WebElement revealed = driver.findElement(By.xpath("//div[@id='authServiceWidget']"));
+    WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='authServiceWidget']")));
+    driver.findElement(By.xpath("//div[@id='authServiceWidget']")).click();
+    }
+}
--- src/test/java/purchaseServiceTest/PurchaseServiceTests.java
+++ src/test/java/purchaseServiceTest/PurchaseServiceTests.java
@@ -2,23 +2,32 @@ package purchaseServiceTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
+import java.time.Duration;
+
 public class PurchaseServiceTests {
+
+    WebDriver driver;
+
+
     @BeforeTest
     public void setUp(){
-        WebDriver driver = new ChromeDriver();
     }
 
     @BeforeMethod
     public void testSetUp(){
-
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://dashboard-2645.trademax-test.com/spa/login");
     }
 
     @AfterTest
     public void tearDown(){
-
+        driver.close();
     }
 }
